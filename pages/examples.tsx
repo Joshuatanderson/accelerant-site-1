740a5abab3b96be104231dd9ab04c530a136f817
Joshua Anderson <joshuatanderson1@gmail.com>
2022-06-07 14:57:16 -0500

    feat: add examples page

---
+++ pages/examples.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { SanityDocument } from "@sanity/client";
+
+import { client } from "../utils/sanity";
+import groq from "groq";
+
+interface Examples {
+  posts: SanityDocument[];
+}
+
+export async function getStaticProps() {
+  const posts = await client
+    .fetch(
+      groq`*[_type == "post" && isPublic == true]{
+        title,
+        slug,
+        excerpt,
+        _createdAt,
+        _id,
+        isPublic,
+        categories[]->{code}
+      }`
+    )
+    .catch(console.error);
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Examples({ posts }: Examples) {
+  console.log(posts);
+  return (
+    <Layout title="Examples" rootPath="/examples">
+      {posts.map((post) => (
+        <article key={post._id}>
+          <header>
+            <h6>
+              <Link href={"/post/[slug]"} as={`/post/${post.slug.current}`}>
+                <a>{post.title}</a>
+              </Link>
+            </h6>
+            <span className="mb-4 text-sm">{post._createdAt}</span>
+          </header>
+          <section>
+            <p className="mb-8">{post.excerpt}</p>
+          </section>
+        </article>
+      ))}
+    </Layout>
+  );
+}
