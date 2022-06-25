5445672b9b69aa355d0e292d74b68c400b2f97ab
Joshua Anderson <joshuatanderson1@gmail.com>
2022-06-25 17:42:18 -0500

    fix(sandbox): button placement

--- components/CodeRunner.tsx
+++ components/CodeRunner.tsx
@@ -70,18 +70,19 @@ const CodeRunner = ({ code }: CodeRunnerProps) => {
 
   return (
     <>
-      <button
-        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={handleRun}
-      >
-        run
-      </button>
-      <button
-        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={handleClearOutput}
-      >
-        clear output
-      </button>
+      <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 flex justify-between">
+        <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none ">
+          <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Run
+          </button>
+        </div>
+        <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+          <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Clear Output
+          </button>
+        </div>
+      </div>
+
       {/* @ts-ignore */}
       <div ref={output} id="output">
         {createConsoleLines(outputText)}
