cb302434b0d71ef6dbd78d22509335a261b33b42
Joshua Anderson <joshuatanderson1@gmail.com>
2022-06-25 17:43:18 -0500

    feat: fix sandbox spacing

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -4,11 +4,10 @@ import Sandbox from "../components/Sandbox";
 const sandbox = () => {
   return (
     <>
-      <div className="mx-auto px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-        {/* <div className="space-y-12"> */}
+      <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <h1 className="text-4xl font-bold leading-tight">Sandbox</h1>
-        {/* </div> */}
       </div>
+
       <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12">
           <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
