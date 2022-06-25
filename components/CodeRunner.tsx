b76414f1e9f9df032e71e95f7d1408cab7ab096a
Joshua Anderson <joshuatanderson1@gmail.com>
2022-06-25 18:20:15 -0500

    feat: style the home page

--- components/CodeRunner.tsx
+++ components/CodeRunner.tsx
@@ -72,12 +72,18 @@ const CodeRunner = ({ code }: CodeRunnerProps) => {
     <>
       <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 flex justify-between">
         <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none ">
-          <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+            onClick={handleRun}
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
             Run
           </button>
         </div>
         <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-          <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+            onClick={handleClearOutput}
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
             Clear Output
           </button>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
+import { ImagesearchRoller } from "@mui/icons-material";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.scss";
+
+const illustrations = [
+  {
+    path: "/illustrations/programming.svg",
+    description: "Python Training",
+  },
+  {
+    path: "/illustrations/team.svg",
+    description: "Project-Based Learning",
+  },
+  {
+    path: "/illustrations/coauthoring.svg",
+    description: "Co-authoring with us",
+  },
+];
 
 const Home: NextPage = () => {
   return (
@@ -15,65 +31,102 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <section>
-        <div className={styles.hero}>
-          <div className={styles.heroText}>
-            <h1>Teach Computer Science in your District</h1>
-            <h2>
+      <section className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Teach Computer Science in your District
+            </h1>
+            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
               Implement equitable CS programs in your district, with dedicated
               support.
-            </h2>
-          </div>
-          <div>
-            <button>
-              <Link href="/contact">Work with us</Link>
-            </button>
-
-            <button>
-              <Link href="/about">Learn more</Link>
-            </button>
+            </p>
           </div>
         </div>
-      </section>
-      <section>
-        <div>
-          <h3>We give your teachers CS superpowers</h3>
-          <h4>
-            Implement equitable CS programs in your district, with dedicated
-            support. No CS degrees required.
-          </h4>
-        </div>
-        <div>
-          <button>
-            <Link href="/contact">Work with us</Link>
-          </button>
+        <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 flex justify-between">
+          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+            <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <Link href="/contact">Work with Us</Link>
+            </button>
+          </div>
+          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none ">
+            <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <Link href="/about">Learn More</Link>
+            </button>
+          </div>
         </div>
       </section>
       <section>
         <div>
-          <h5>We train your teachers</h5>
-          <p>
-            You don&apos;t need a CS degree to teach the basics. We&apos;ll
-            train your teachers, so they have the skills they need.
-          </p>
-        </div>
-        <div>
-          <h5>Credentials for students</h5>
-          <p>
-            Strengthen your high school&apos;s diploma by preparing students for
-            high-paying software engineering jobs.
-          </p>
-        </div>
-        <div>
-          <h5>Equity Oriented</h5>
-          <p>
-            We are committed to doing whatever it takies to empower your
-            students. Everyone deserves a chance to code, regardless of race,
-            gender, or status.
-          </p>
+          <div className="bg-white">
+            <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+              <div className="space-y-12">
+                <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+                  <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                    We give your teachers CS superpowers
+                  </h2>
+                  <p className="text-xl text-gray-500">
+                    Implement equitable CS programs in your district, with
+                    dedicated support. No CS degrees required.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <section>
+        <div>
+          <div className="bg-white">
+            <div className="mx-auto py-6 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+              <div className="space-y-12">
+                <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+                  <h5 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                    We train your teachers
+                  </h5>
+                  <p className="text-xl text-gray-500">
+                    You don&apos;t need a CS degree to teach the basics.
+                    We&apos;ll train your teachers, so they have the skills they
+                    need.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white">
+            <div className="mx-auto py-6 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+              <div className="space-y-12">
+                <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+                  <h5 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                    Credentials for students
+                  </h5>
+                  <p className="text-xl text-gray-500">
+                    Strengthen your high school&apos;s diploma by preparing
+                    students for high-paying software engineering jobs.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white">
+            <div className="mx-auto py-6 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+              <div className="space-y-12">
+                <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+                  <h5 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                    Equity Oriented
+                  </h5>
+                  <p className="text-xl text-gray-500">
+                    We are committed to doing whatever it takies to empower your
+                    students. Everyone deserves a chance to code, regardless of
+                    race, gender, or status.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <section>
         <div>
           <h6>How did it start?</h6>
           <p>
@@ -96,37 +149,52 @@ const Home: NextPage = () => {
           <div></div>
           Joshua Anderson, founder of Accelerant
         </div>
-      </section>
+      </section> */}
       <section>
-        <h6>Want to lead the way in code literacy?</h6>
-        <p>
-          We are currently looking for districts and schools who are excited
-          about:
-        </p>
-        <div>
-          <Image
-            src="/illustrations/programming.svg"
-            width={100}
-            height={100}
-          ></Image>
-          Python Training
+        <div className="bg-white">
+          <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+            <div className="space-y-12">
+              <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+                <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+                  Want to lead the way in code literacy?
+                </h2>
+                <p className="text-xl text-gray-500">
+                  We are currently looking for districts and schools who are
+                  excited about:
+                </p>
+              </div>
+              <ul
+                role="list"
+                className="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl"
+              >
+                {illustrations.map((illustration) => (
+                  <li key={illustration.description}>
+                    <div className="space-y-6">
+                      <img
+                        className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                        src={illustration.path}
+                        alt={illustration.description}
+                      />
+                      <div className="space-y-2">
+                        <div className="text-lg leading-6 font-medium space-y-1">
+                          <h3>{illustration.description}</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-        <div>
-          <Image src="/illustrations/team.svg" width={100} height={100}></Image>
-          Project-Based Learning
-        </div>
-        <div>
-          <Image
-            src="/illustrations/coauthoring.svg"
-            width={100}
-            height={100}
-          ></Image>
-          Co-authoring with us
-        </div>
-        <div>
-          <button>
-            <Link href="/contact">Want to know more?</Link>
-          </button>
+        <h6></h6>
+        <p></p>
+        <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 flex justify-center">
+          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+            <button className="px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <Link href="/contact">Want to know more?</Link>
+            </button>
+          </div>
         </div>
       </section>
 
