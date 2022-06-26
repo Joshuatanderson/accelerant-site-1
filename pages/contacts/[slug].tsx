e5b4e799c3897cdbf325222964d7b67432caf91f
Joshua Anderson <joshuatanderson1@gmail.com>
2022-06-25 23:52:49 -0500

    feat: update contacts

--- pages/contacts/[slug].tsx
+++ pages/contacts/[slug].tsx
@@ -37,18 +37,33 @@ export default function Contact({ display, code, slug }: ContactProps) {
 
   return (
     <div>
-      <h1>{display}</h1>
-      <p>
-        A contact card should automatically download. If not, please click
-        below.
-      </p>
-      <button>
-        {/* <Link> */}
-        <a download={display} ref={downloadLink} href={`/cards/${code}.vcf`}>
-          Download
-        </a>
-        {/* </Link> */}
-      </button>
+      <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <h1 className="text-4xl font-bold leading-tight">{display}</h1>
+      </div>
+
+      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <div className="space-y-12">
+          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+            <p className="text-xl text-gray-500">
+              A contact card should automatically download. If not, please click
+              below.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto py-4 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 flex justify-between">
+        <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none ">
+          <button className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aDark hover:bg-aPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <a
+              download={display}
+              ref={downloadLink}
+              href={`/cards/${code}.vcf`}
+            >
+              Download
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
